2f2fb44006a4ea658cde90675d325b751a51634e
nhayes-roth <nathan.hayesroth@gmail.com>
2013-10-08 01:22:57 -0400

    Server mostly done, need to work on ClientThreads

--- Server.java
+++ Server.java
@@ -1,99 +1,171 @@
 /*
  * File: Server.java
  * ------------
- * Name : Nathan Hayes-Roth
- * UNI : nbh2113
- * Class: Computer Networks
+ * Name:    Nathan Hayes-Roth
+ * UNI:    nbh2113
+ * Class:    Computer Networks
  * Assignment: Programming Assignment #1
  * ------------
  * Description:
  * 
  */
 
-import java.net.*;
 import java.io.*;
+import java.net.*;
 import java.util.Hashtable;
+import java.util.Vector;
+import java.util.HashSet;
 
 
 public class Server {
-	// private variables
-	private Socket          client_socket   = null;
-	private ServerSocket    server_socket   = null;
-	private DataInputStream streamIn =  null;
 
-	// server constructor
-	public Server(int port_number) {  
-		try {
-			// create a soocket that listens to connection requests
-			System.out.println("Binding to port " + port_number + ", please wait  ...");
-			server_socket = new ServerSocket(port_number);
-			System.out.println("Server started: " + server_socket);
-			System.out.println("Waiting for a client..."); 
-			// listen for a connection to be made and accept it
-			client_socket = server_socket.accept();
-			System.out.println("Client accepted: " + client_socket);
-			open();
-			boolean done = false;
-			while (!done){
-				try {
-					String line = streamIn.readUTF();
-					System.out.println(line);
-					done = line.equals(".bye");
-				}
-				catch(IOException ioe){
-					done = true;
-				}
-			}
-			close();
-		}
-		catch(Exception e) {
-			System.out.println("Exception: " + e.getMessage());
-		}
-	}
+    /* Class Variables */
+    private static ServerSocket                     server_socket = null;
+    private static Socket                           client_socket = null;
+    private static final int                      maxClientsCount = 10;
+    private static final ClientThread[]                   threads = new ClientThread[maxClientsCount];
+    public static Vector<ClientThread>       synchronized_threads = new Vector<ClientThread>();             // Synchronized storage prevents errors such as null pointers
+    private static Hashtable<String, String>          credentials = new Hashtable<String,String>();
+    private static Hashtable<InetAddress, Long> blocked_addresses = new Hashtable<InetAddress, Long>();     // Synchronized storage prevents errors such as null pointers
+    
+    /*
+     * Check command line arguments for correct length. If necessary,
+     * instruct the user how to correctly execute the program. Default values
+     * provided.
+     */
+    private static void setupServer(String[] args, int port_number){
+        // check command line arguments for correct length and select port
+        if (args.length != 1){
+            System.out.println("\n       Usage: java Server <port_number>");
+            System.out.println("Defaulted to: java Server 4119\n");
+        } else {
+            port_number = Integer.parseInt(args[0]);
+        }
+        // open a server socket to listen for connections on
+        try {
+            server_socket = new ServerSocket(port_number);
+        } catch (Exception e) {
+            System.err.println("Error creating server socket: " + e.getMessage());
+        }
+        
+    }
+    
+    /* 
+     * Load credentials from text file into Hashtable
+     */
+    private static Hashtable<String, String> loadCredentials() {
+        try {
+            @SuppressWarnings("resource")
+            BufferedReader reader = new BufferedReader(new FileReader("./credentials.txt"));
+            String line = null;
+            // split each line and add the name/password pair to the hashtable
+            while ((line = reader.readLine()) != null) {
+                String[] split = line.split("\\s");
+                credentials.put(split[0], split[1]);
+            }
+        } catch (Exception e) {
+            System.out.println("Error loading credentials: " + e.getMessage());
+        }
+        return credentials;
+    }
 
-	// open a connection
-	public void open() throws IOException {
-		streamIn = new DataInputStream(new BufferedInputStream(client_socket.getInputStream()));
-	}
+    /*
+     * Listen for new client connections and treat them accordingly.
+     */
+    private static void listen() {
+        // loop indefinitely
+        while (true) {
+            try {
+                // listen for a connection and accept it
+                client_socket = server_socket.accept();
+                // check if the address is banned
+                if (!vetClient(client_socket, 10)){
+                    // let the bad man know what he did
+                    PrintStream os = new PrintStream(client_socket.getOutputStream());
+                    os.println("\nYou have been temporarily banned from the server. Please try again later.\n");
+                    os.close();
+                    client_socket.close();
+                } else {
+                    //synchronized_threads.add(new ClientThread(client_socket, threads));
+                    
+                    
+                    blocked_addresses.put(client_socket.getInetAddress(), System.nanoTime());
+                    
+                    int i = 0;
+                    for (i = 0; i < maxClientsCount; i++) {
+                        if (threads[i] == null) {
+                            (threads[i] = new ClientThread(client_socket, threads))
+                                    .start();
+                            break;
+                        }
+                    }
+                    if (i == maxClientsCount) {
+                        PrintStream os = new PrintStream(
+                                client_socket.getOutputStream());
+                        os.println("Server too busy. Try later.");
+                        os.close();
+                        client_socket.close();
+                    }
+                    
+                    
+                }
+            } catch (Exception e) {
+                System.err.println("Error creating a ClientThread: " + e.getMessage());
+            }
+                
+                /*
+                
+                blocked_addresses.put(client_socket.getInetAddress(), System.nanoTime());
+                
+                int i = 0;
+                for (i = 0; i < maxClientsCount; i++) {
+                    if (threads[i] == null) {
+                        (threads[i] = new ClientThread(client_socket, threads))
+                                .start();
+                        break;
+                    }
+                }
+                if (i == maxClientsCount) {
+                    PrintStream os = new PrintStream(
+                            client_socket.getOutputStream());
+                    os.println("Server too busy. Try later.");
+                    os.close();
+                    client_socket.close();
+                }
+                
+                
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+            
+                */
+        }
+        
+    }
+    
+    /*
+     * Check to see if a client socket is on the list of banned addresses.
+     * If it is, check to see if enough time has passed.
+     */
+    private static boolean vetClient(Socket socket, int enough_time) {
+        InetAddress address = socket.getInetAddress();
+        // if the address is blocked
+        if (blocked_addresses.containsKey(address)){
+            // if enough time has passed
+            long elapsed_time = System.nanoTime() - blocked_addresses.get(address);
+            double seconds = (double)elapsed_time / 1000000000.0;
+            if (seconds > enough_time){
+                // remove it from the list and return true
+                blocked_addresses.remove(address);
+                return true;
+            } else return false;
+        } else return true;
+    }
 
-	// close a connection
-	public void close() throws IOException {
-		if (client_socket != null)
-			client_socket.close();
-		if (streamIn != null)
-			streamIn.close();
-	}
+    public static void main(String args[]) {
+        loadCredentials();
+        setupServer(args, 4119);
+        listen();
+    }
 
-	// load credentials from text file into Hashtable
-	public static Hashtable<String, String> loadCredentials(){
-		Hashtable<String, String> to_return = new Hashtable<String, String>();
-		try {
-			// open a buffered reader
-			BufferedReader reader = new BufferedReader(new FileReader("./credentials.txt"));
-			String line = null;
-			// split each line and add the name/password pair to the hashtable
-			while ((line = reader.readLine()) != null) {
-				String[] split = line.split("\\s");
-				to_return.put(split[0], split[1]);
-			}
-		}
-		catch (Exception e) {
-			System.out.println("Error loading credentials: " + e.getMessage());
-		}
-		return to_return;
-	}
-
-	// main
-	public static void main(String args[]) {
-		Server server = null;							// server
-		Hashtable credentials = loadCredentials();		// read credentials into Hashtable
-		System.out.println("DEBUG: " + credentials.size());
-
-		if (args.length != 1){
-			System.out.println("\nUsage: java Server port_number");
-			System.out.println("e.g.   java Server 4119\n");
-		}
-		else
-			server = new Server(Integer.parseInt(args[0]));
-	}
 }
