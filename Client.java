c479a01710ef7b3f49973e31aff46b5b1ae5e57e
nhayes-roth <nathan.hayesroth@gmail.com>
2013-10-07 22:38:27 -0400

    Client cleaned up, 1 thread for reading, 1 for writing

--- Client.java
+++ Client.java
@@ -1,116 +1,105 @@
 /*
  * File: Client.java
  * ------------
- * Name : Nathan Hayes-Roth
- * UNI : nbh2113
- * Class: Computer Networks
+ * Name:    Nathan Hayes-Roth
+ * UNI:    nbh2113
+ * Class:    Computer Networks
  * Assignment: Programming Assignment #1
  * ------------
- * Description:
- * 
+ * Description: The client application allows users to log onto the server using the
+ * client terminal. The client application opens a TCP connection to the server and 
+ * then runs two threads simultaneously, allowing the user to write to as well as 
+ * read from the server.
  */
 
-import java.net.*;
 import java.io.*;
+import java.net.*;
 
 public class Client implements Runnable {
-	
-	/* class variables */
-	private Socket 			 client_socket = null;
-	private DataInputStream  input_stream  = null;
-	private DataOutputStream output_stream = null;
-	private boolean			 closed		   = false;
 
-	/* Client Constructor */
-	public Client(String server_address, int port_number) {
-		System.out.println("Establishing connection. Please wait...");
-		// open a socket for communication with the server
-		try {
-			// port_number is the port number the server is listening on
-			client_socket = new Socket(server_address, port_number);
-			System.out.println("Connected: " + client_socket);
-			start();
-		}
-		// catch exceptions
-		catch(Exception e) {
-			System.out.println("Error opening client socket: " + e.getMessage());
-		}
-		// read client input and send it to outpur stream
-		try {
-			// open a buffered reader
-			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-			String line = "";
-			// split each line and add the name/password pair to the hashtable
-			while ((line = reader.readLine()) != null && !line.equals("logout")) {
-				output_stream.writeUTF(line);
-				output_stream.flush();
-			}
-		}
-		catch (IOException e) {
-			System.out.println("Error reading client input : " + e.getMessage());
-		}
-	}
+    /* Class Variables */
+    private static Socket         client_socket = null;  // socket to connect to server
+    private static BufferedReader  input_stream = null;  // input from server
+    private static PrintStream    output_stream = null;  // output to server
+    private static boolean               closed = false; // boolean signal if client has logged out
+    private static BufferedReader  client_input = null;  // input from client
 
-	// setup a new ouput stream
-	public void start() throws IOException{
-		input_stream   = new DataInputStream(System.in);
-		output_stream = new DataOutputStream(client_socket.getOutputStream());
-	}
+    /*
+     * Check command line arguments for correct length. If necessary,
+     * instruct the user how to correctly execute the program. Default values
+     * provided.
+     */
+    private static void setupConnection(String[] args, String server_address, int port_number){
+        // check command line arguments for correct length and select address/port
+        if (args.length != 2){
+            System.out.println("\nUsage:        java Client <server_address> <port_number>");
+            System.out.println("Defaulted to: java Client localhost 4119\n");
+        } else {
+            server_address = args[0];
+            port_number = Integer.parseInt(args[1]);
+        }
+        // make connection
+        try {
+            client_socket = new Socket(server_address, port_number);
+            client_input  = new BufferedReader(new InputStreamReader(System.in));
+            output_stream = new PrintStream(client_socket.getOutputStream());
+            input_stream  = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
+            client_input  = new BufferedReader(new InputStreamReader(System.in));
+        } catch (Exception e) {
+            System.err.println("Error opening client socket: " + e.getMessage());
+            System.exit(1);
+        }
+        
+    }
+    
+    /*
+     * Manage the sending and receiving of information once the client has connected.
+     */
+    private static void manageConnection(){
+        // verify socket and socket streams were initialized
+        if (client_socket == null || output_stream == null || input_stream == null){
+            System.err.println("Socket object never initialized.");
+        } else {
+            try {
+                // create a thread to read from the server (calls run())
+                new Thread(new Client()).start();
+                // write to the server until the connection is closed
+                while (!closed) {
+                    output_stream.println(client_input.readLine());
+                }
+                // cleanup
+                output_stream.close();
+                input_stream.close();
+                client_socket.close();
+                System.exit(1);
+            } catch (IOException e) {
+                System.err.println("IOException:  " + e);
+            }
+        }
+    }
+    
+    /*
+     * Create a thread to read from the server. Close the connection if the Server 
+     * gives the signal.
+     */
+    public void run() {
+        String server_message;
+        try {
+            while ((server_message = input_stream.readLine()) != null) {
+                System.out.println(server_message);
+                if (server_message.indexOf("*** Bye") != -1)
+                    break;
+            }
+            closed = true;
+        } catch (Exception e) {
+            System.err.println("Error reading from server:  " + e.getMessage());
+            System.exit(1);
+        }
+    }
 
-	// 
-	public void stop() {
-		try {
-			if (input_stream   != null)  input_stream.close();
-			if (output_stream != null)  output_stream.close();
-			if (client_socket    != null)  client_socket.close();
-		}
-		catch(IOException ioe){
-			System.out.println("Error closing ...");
-		}
-	}
-
-	/*
-	 * Create a thread to read data from the server.
-	 */
-	public void run(){
-		try {
-			// open a buffered reader
-			BufferedReader reader = new BufferedReader(new InputStreamReader(input_stream));
-			String line = "";
-			// split each line and add the name/password pair to the hashtable
-			while ((line = reader.readLine()) != null) {
-				System.out.println(line);
-				if (!line.equals("logout")){
-					break;
-				}
-			}
-			closed = true;
-		}
-		catch (IOException e) {
-			System.out.println("Error reading client input : " + e.getMessage());
-		}
-	}
-
-	private static void instruct(){
-		System.out.println("\nUsage: java Client <server_address> <port_number>");
-		System.out.println("e.g.   java Client localhost 4119\n");
-	}
-
-	public static void main(String args[]) {
-		// check cmd line arguments for correct length
-		if (args.length != 2){
-			instruct();
-		}
-		// create a client
-		else {
-			try{
-				Client client = new Client(args[0], Integer.parseInt(args[1]));
-			}
-			// catch formatting exceptions and instruct the user
-			catch (Exception e) {
-				System.out.println("Command line error: " + e.getMessage());
-				instruct();
-			}
-		}
-	}
+    /* main */
+    public static void main(String[] args) {
+        setupConnection(args, "localhost", 4119);
+        manageConnection();
+    }
 }
